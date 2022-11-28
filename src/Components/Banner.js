ed8015954197cf7998180ebdcb4ec776fa8ca45a
Madhumitha <madhumithakp@gmail.com>
2022-11-28 18:45:25 +0530

    adding html of netflix

---
+++ src/Components/Banner.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Banner.css"
+
+function Banner() {
+  return (
+    <div className='banner'>
+        <div className='content'>
+            <h1 className='title'>Movie Name</h1>
+            <div className='banner_buttons'>
+                <botton className="button">Play</botton>
+                <button className='button'>My List</button>
+            </div>
+            <h1 className='description'>In publishing and graphic design uiuhu euhuehufe ehfejhygc cftctfy ytgftrdrt</h1>
+        </div>
+        <div className="fade_bottom"></div>
+
+
+      
+    </div>
+  )
+}
+
+export default Banner

e8f1b364d9314547ce838fdec5e65c065fa153b7
Madhumitha <madhumithakp@gmail.com>
2022-11-30 12:24:25 +0530

    background added

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,22 +1,25 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Banner.css"
-import { API_KEY } from '../Constants/Constant'
-import axios from "./Components/axios"
+import { API_KEY,imageUrl } from '../Constants/Constant'
+import axios from "./axios";
 function Banner() {
+    const[movie,setMovie]=useState()
     useEffect(()=>{
-        axios.get("trending/all/week?api_key=${API_KEY}&language=en-US").then((response)=>{
-            console.log(response.data)
+        axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+            console.log(response.data.results[0])
+            setMovie(response.data.results[1])
         })
     },[])
   return (
-    <div className='banner'>
+    <div style={{backgrountImage:`url(${movie?imageUrl+movie.backdrop_path:""})`}}
+     className='banner'>
         <div className='content'>
-            <h1 className='title'>Movie Name</h1>
+            <h1 className='title'>{movie?movie.title:""}</h1>
             <div className='banner_buttons'>
                 <botton className="button">Play</botton>
                 <button className='button'>My List</button>
             </div>
-            <h1 className='description'>In publishing and graphic design uiuhu euhuehufe ehfejhygc cftctfy ytgftrdrt</h1>
+            <h1 className='description'>{movie?movie.overview:""}</h1>
         </div>
         <div className="fade_bottom"></div>
     </div>
--- src/Components/axios.js
+++ src/Components/axios.js
@@ -1,5 +1,5 @@
-import axios from axios;
-import {baseUrl} from "./Constants/Constant";
+import axios from "axios";
+import {baseUrl} from "../Constants/Constant";
 const instance = axios.create({
     baseURL: baseUrl,
   });
